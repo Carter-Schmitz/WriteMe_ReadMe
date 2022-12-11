f581a957f88aa1eb6a3925a234fccb7f1ee7af73
Carter_Schmitz <carterschmitz95@gmail.com>
2022-12-10 18:24:49 -0600

    added writefile function using input

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,6 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
 const inquirer = require('inquirer')
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
 function init() {
@@ -55,7 +50,12 @@ function init() {
             type: 'input',
             name: 'email',
             message: 'What is your email address?',
-        }])
+        },
+    ])
+    .then((data) => {
+        fs.writeFile('README.md', data, (err) => 
+        err ? console.log(err) : console.log('Successfully Created ReadMe!')) 
+    })
 }
 
 // Function call to initialize app
